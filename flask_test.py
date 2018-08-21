4adfd42204fac51d76794613223f4169c62e36bc
Shrey Batra <shreybatra97@gmail.com>
2018-08-21 22:14:34 +0530

    added files

---
+++ flask_test.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, send_file
+import addressbook_pb2
+import io
+
+app = Flask(__name__)
+
+def ListPeople(address_book):
+    data = []
+    for person in address_book.people:
+        person_data = {}
+        person_data['id'] = person.id
+        person_data['name'] = person.name
+        
+        if person.HasField('email'):
+            person_data['email'] = person.email
+
+        phone_numbers = []
+
+        for phone_number in person.phones:
+            phone = {}
+            if phone_number.type == addressbook_pb2.Person.MOBILE:
+                phone['type'] = 'Mobile'
+            elif phone_number.type == addressbook_pb2.Person.HOME:
+                phone['type'] = 'Home'
+            elif phone_number.type == addressbook_pb2.Person.WORK:
+                phone['type'] = 'Work'
+            phone['number'] = phone_number.number
+            phone_numbers.append(phone)
+
+        person_data['phones'] = phone_numbers
+
+        data.append(person_data)
+
+    return data
+
+
+@app.route('/load', methods=['POST'])
+def hello():
+    address_book = addressbook_pb2.AddressBook()
+    
+    address_book.ParseFromString(request.data)
+
+    print(address_book)
+
+    return send_file(
+        io.BytesIO(address_book.SerializeToString()),
+        as_attachment=True,
+        attachment_filename='abc.abc',
+        mimetype='attachment/x-protobuf'
+        )
+
+
+if __name__=='__main__':
+    app.run(debug=True)
