3e969c8fcdd7051f2cee698f4133b194e24621c1
keshav2904 <36502001+keshav2904@users.noreply.github.com>
2018-10-02 11:16:58 +0530

    update make_proto_file.py and read_display_proto_file.py (#4)
    * Update make_proto_file.py
    
    * Update read_display_proto_file.py
    
    * Update make_proto_file.py

--- make_proto_file.py
+++ make_proto_file.py
@@ -5,22 +5,22 @@ import sys
 
 # This function fills in a Person message based on user input.
 def PromptForAddress(person):
-  person.id = int(raw_input("Enter person ID number: "))
-  person.name = raw_input("Enter name: ")
+  person.id = int(input("Enter person ID number: "))
+  person.name = input("Enter name: ")
 
-  email = raw_input("Enter email address (blank for none): ")
+  email = input("Enter email address (blank for none): ")
   if email != "":
     person.email = email
 
   while True:
-    number = raw_input("Enter a phone number (or leave blank to finish): ")
+    number = input("Enter a phone number (or leave blank to finish): ")
     if number == "":
       break
 
     phone_number = person.phones.add()
     phone_number.number = number
 
-    type = raw_input("Is this a mobile, home, or work phone? ")
+    type = input("Is this a mobile, home, or work phone? ")
     if type == "mobile":
       phone_number.type = addressbook_pb2.Person.MOBILE
     elif type == "home":
@@ -28,13 +28,13 @@ def PromptForAddress(person):
     elif type == "work":
       phone_number.type = addressbook_pb2.Person.WORK
     else:
-      print "Unknown phone type; leaving as default value."
+      print ("Unknown phone type; leaving as default value.")
 
 # Main procedure:  Reads the entire address book from a file,
 #   adds one person based on user input, then writes it back out to the same
 #   file.
 if len(sys.argv) != 2:
-  print "Usage:", sys.argv[0], "ADDRESS_BOOK_FILE"
+  print ("Usage:", sys.argv[0], "ADDRESS_BOOK_FILE")
   sys.exit(-1)
 
 address_book = addressbook_pb2.AddressBook()
@@ -45,7 +45,7 @@ try:
   address_book.ParseFromString(f.read())
   f.close()
 except IOError:
-  print sys.argv[1] + ": Could not open file.  Creating a new one."
+  print (sys.argv[1] + ": Could not open file.  Creating a new one.")
 
 # Add an address.
 PromptForAddress(address_book.people.add())
